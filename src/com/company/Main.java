055ed974f20454bccbedf6d67ad1b24dcb9f1de7
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-10-18 14:59:21 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
-    private static Object lock = new Object();
+    private static ReentrantLock lock = new ReentrantLock(true);
 
     public static void main(String[] args) {
 
@@ -36,8 +38,11 @@ public class Main {
         @Override
         public void run() {
             for (int i = 0; i < 100; i++) {
-                synchronized (lock){
+                lock.lock();
+                try {
                     System.out.format(threadColor + "%s: runCount = %d\n ", Thread.currentThread().getName(), runCount++);
+                } finally {
+                    lock.unlock();
                 }
             }
         }
